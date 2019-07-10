5bb85affdf8f0ac18f38b422f74df584b36d05b6
Brendon Kay <bak04280@gmail.com>
2019-07-09 22:38:07 -0600

    Update some prints

--- chapter_2/test_exercise_1.py
+++ chapter_2/test_exercise_1.py
@@ -18,27 +18,33 @@ class TestStringMethods(unittest.TestCase):
         print("Creating Test List")
         self._tl: TestList = generate_random_testlist()
 
-    def test_linear_contains(self):
+    def test_binary_contains(self):
         """
-        Test chapter_2.exercise_1.linear_contains
+        Test chapter_2.exercise_1.binary_contains
         This test runs the function 5 times using a randomly generated key to
         find in a randomly generated list of one million integers that range
-        from 1 to 100
+        from 1 to 100000
         """
         for i in range(1,5):
             key = random.choice(range(1, 100000))
-            print(f"Using linear search to find random key: {key}")
-            results, hop_count = linear_contains(self._tl, key)
+            print(f"Using binary search to find random key: {key}")
+            results, hop_count = binary_contains(self._tl, key)
             if results:
                 print(f"Found key {key} in {hop_count} hops")
             else:
                 print(f"Key {key} was not found in {hop_count} hops")
 
-    def test_binary_contains(self):
+    def test_linear_contains(self):
+        """
+        Test chapter_2.exercise_1.linear_contains
+        This test runs the function 5 times using a randomly generated key to
+        find in a randomly generated list of one million integers that range
+        from 1 to 100000
+        """
         for i in range(1,5):
             key = random.choice(range(1, 100000))
-            print(f"Using binary search to find random key: {key}")
-            results, hop_count = binary_contains(self._tl, key)
+            print(f"Using linear search to find random key: {key}")
+            results, hop_count = linear_contains(self._tl, key)
             if results:
                 print(f"Found key {key} in {hop_count} hops")
             else:
